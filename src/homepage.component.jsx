4d83b00cb0a3de24fdba77792fb14c8522bd8da0
PrativaG <c7.paru@gamil.com>
2020-07-16 10:29:51 -0700

    initial commit with small homepage  feature

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                        <h1  className="title">Spoons</h1>
+                        <span className="subtitle">Shop Now</span>
+                </div>
+            </div>
+         <div className="menu-item">
+             <div className="content">
+                     <h1  className="title">Bongs</h1>
+                     <span className="subtitle">Shop Now</span>
+             </div>
+         </div>
+    
+ 
+  
+      <div className="menu-item">
+          <div className="content">
+                  <h1  className="title">Bubblers</h1>
+                  <span className="subtitle">Shop Now</span>
+          </div>
+      </div>
+     <div className="menu-item">
+         <div className="content">
+                 <h1  className="title">Chillums</h1>
+                 <span className="subtitle">Shop Now</span>
+         </div>
+     </div>
+     <div className="menu-item">
+         <div className="content">
+                 <h1  className="title">Dab Rigs</h1>
+                 <span className="subtitle">Shop Now</span>
+         </div>
+     </div>
+     </div>
+ </div>
+   
+    
+);
+export default HomePage;
