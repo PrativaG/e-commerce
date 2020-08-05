d329efdd98d3fa5bade851f8f8698233e30e93ca
PrativaG <c7.paru@gamil.com>
2020-08-05 09:45:58 -0700

    updated menu item with withRouter HOC and started shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const SpoonPage = (props) => {
+  console.log(props)
+  return(
+   
+    <div>
+      <Link to='/'>Go to home</Link>
+      <h1>Spoon Page</h1>
+  </div>
+  );
+  };
+
 
 function App() {
   return (
     <div >
-     <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/spoons' component={SpoonPage}/>
+      </Switch>
+    
+
     </div>
   );
 }
--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './directory.style.scss';
 import MenuItem from '../menu-item/menu-item.component';
+
 class Directory extends React.Component {
     constructor(){
         super(); //to pull every thing form react
@@ -8,33 +9,38 @@ class Directory extends React.Component {
             sections: [{
                 title : 'spoons',
                 imageurl : 'https://weedmaps.com/learn/wp-content/uploads/2019/08/181016_Learn_Pipe_29.jpg',
-                id : 1
+                id : 1,
+                linkUrl : 'spoons'
             },
 
             {
                 title : 'bongs',
                 imageurl : 'https://weedmaps.com/learn/wp-content/uploads/2019/08/180918_Bong_05.jpg',
-                id : 2
+                id : 2,
+                linkUrl : ''
             },
 
             {
                 title : 'bubblers',
                 imageurl : 'https://weedmaps.com/learn/wp-content/uploads/2019/08/181008_OutdoorSesh140.jpg',
-                id : 3
+                id : 3,
+                linkUrl : ''
             },
 
             {
                 title : 'chillums',
                 imageurl : 'https://weedmaps.com/learn/wp-content/uploads/2019/08/shutterstock_1417424039-1.jpg',
                 size : 'large',
-                id : 4
+                id : 4,
+                linkUrl : ''
             },
 
             {
                 title : 'dab rigs',
                 imageurl : 'https://weedmaps.com/learn/wp-content/uploads/2019/08/180918_Dab_03.jpg',
                 size : 'large',
-                id : 5
+                id : 5,
+                linkUrl : ''
             },
             
         ]
@@ -44,8 +50,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className="directory-menu">
-                {this.state.sections.map(({title, imageurl, id, size}) => (
-                    <MenuItem key = {id} title = {title} imageUrl={imageurl} size = {size}/>
+                {this.state.sections.map(({id, imageUrl, ...otherSectionProps}) => (
+                    <MenuItem key = {id} {...otherSectionProps}/>
                 ))}
             </div>
         )
